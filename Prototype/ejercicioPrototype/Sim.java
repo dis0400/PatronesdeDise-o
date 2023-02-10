27376ab33b5996e4e86b3ee77d29b87ff543fb8f
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-02-10 13:58:45 -0800

    Create Sim.java

---
+++ Prototype/ejercicioPrototype/Sim.java
@@ -0,0 +1,22 @@
+public class Sim {
+    private String nombreEmpresaTel;
+    private String numeroTelefono;
+
+    public Sim(String nombreEmpresaTel, String numeroTelefono){
+        this.nombreEmpresaTel=nombreEmpresaTel;
+        this.numeroTelefono=numeroTelefono;
+    }
+
+    public String getNombreEmpresaTel() {
+        return nombreEmpresaTel;
+    }
+    public void setNombreEmpresaTel(String nombreEmpresaTel) {
+        this.nombreEmpresaTel = nombreEmpresaTel;
+    }
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+}
