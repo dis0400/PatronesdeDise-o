9f40a8699ab22d4fbdc69b985e2cacf16909a40c
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-02-13 13:33:06 -0800

    Update ConcreteCreatorPasajeInfantil.java

--- FactoryMethod/ConcreteCreatorPasajeInfantil.java
+++ FactoryMethod/ConcreteCreatorPasajeInfantil.java
@@ -5,11 +5,11 @@ public class ConcreteCreatorPasajeInfantil extends CreatorPasaje {
     @Override
     public IPasaje createPasaje(Pasajero pasajero, String numeroAsiento) {
         PasajeInfantil pasaje = new PasajeInfantil();
-        pasaje.setAvion(new Avion("MOPAR", "1 ton", "AK-45", 100));
-        pasaje.setCostoEspecial("30 US");
-        pasaje.setOrigen(new Lugar("Japon", "Tokyo", "Aeropueto Internacional de Tokyo"));
-        pasaje.setDestino(new Lugar("Irlanda", "Dublin", "Aeropuerto Internacional de Dublin"));
-        pasaje.setNumeroVuelo("123AJ");
+        pasaje.setAvion(new Avion("MOR", "1 ton", "AY70", 100));
+        pasaje.setCostoEspecial("40 US");
+        pasaje.setOrigen(new Lugar("Alemania", "Berlin", "Aeropueto Internacional de Alemania"));
+        pasaje.setDestino(new Lugar("Argentina", "Cordoba", "Aeropuerto Internacional de Argentina"));
+        pasaje.setNumeroVuelo("345B");
         pasaje.setNumeroAsiento(numeroAsiento);
         pasaje.setPasajero(pasajero);
         return pasaje;
