da0b56368cac600bcbba1e37623f2d46e3a5fec4
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-02-13 13:48:34 -0800

    Update Main.java

--- FactoryMethod/Main.java
+++ FactoryMethod/Main.java
@@ -2,14 +2,14 @@ package FactoryMethod;
 
 public class Main {
     public static void main(String[] args) {
-        PasajeStandard s1 = (PasajeStandard) new ConcreteCreatorPasajeStandard().createPasaje(new Pasajero("Josue Nazaret", "251200"), "A1");
-        PasajeStandard s2 = (PasajeStandard) new ConcreteCreatorPasajeStandard().createPasaje(new Pasajero("Alba Nazaret", "861200"), "A2");
+        PasajeStandard s1 = (PasajeStandard) new ConcreteCreatorPasajeStandard().createPasaje(new Pasajero("Valentina La Faye", "130886"), "A1");
+        PasajeStandard s2 = (PasajeStandard) new ConcreteCreatorPasajeStandard().createPasaje(new Pasajero("Diego Moron", "300457"), "A2");
 
-        PasajeSolidario so1 = (PasajeSolidario) new ConcreteCreatorPasajeSolidario().createPasaje(new Pasajero("Jose Luis Roncha", "126232"), "F3");
-        PasajeSolidario so2 = (PasajeSolidario) new ConcreteCreatorPasajeSolidario().createPasaje(new Pasajero("Luis Miguel Roncha", "126227"), "J3");
+        PasajeSolidario so1 = (PasajeSolidario) new ConcreteCreatorPasajeSolidario().createPasaje(new Pasajero("Mariel Soria", "88932"), "F3");
+        PasajeSolidario so2 = (PasajeSolidario) new ConcreteCreatorPasajeSolidario().createPasaje(new Pasajero("Fabian Sanchez", "273290"), "G3");
 
-        PasajeInfantil i1 = (PasajeInfantil) new ConcreteCreatorPasajeInfantil().createPasaje(new Pasajero("Roberto Sanjinez", "1237824"), "H2");
-        PasajeInfantil i2 = (PasajeInfantil) new ConcreteCreatorPasajeInfantil().createPasaje(new Pasajero("Aquiles Illanes", "122134784"), "G5");
+        PasajeInfantil i1 = (PasajeInfantil) new ConcreteCreatorPasajeInfantil().createPasaje(new Pasajero("Rodrigo Limpias", "134791"), "L2");
+        PasajeInfantil i2 = (PasajeInfantil) new ConcreteCreatorPasajeInfantil().createPasaje(new Pasajero("Gabriel Tapia", "1349032"), "L5");
 
         s1.show();
         s2.show();
