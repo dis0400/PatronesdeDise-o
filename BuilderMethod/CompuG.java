2b790adf8e904559cd0e2a6f077051ed27b01e5a
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-02-15 10:01:11 -0800

    Create CompuG.java

---
+++ BuilderMethod/CompuG.java
@@ -0,0 +1,74 @@
+package BuilderMethod;
+
+public class Computadora {
+    private String nombre;
+    private String monitor;
+    private String teclado;
+    private String mouse;
+    private String memoria;
+    private String procesador;
+    private String video;
+
+    public String getMonitor() {
+        return monitor;
+    }
+    public Computadora setMonitor(String monitor) {
+        this.monitor = monitor;
+        return this;
+    }
+    public String getTeclado() {
+        return teclado;
+    }
+    public Computadora setTeclado(String teclado) {
+        this.teclado = teclado;
+        return this;
+    }
+    public String getMouse() {
+        return mouse;
+    }
+    public Computadora setMouse(String mouse) {
+        this.mouse = mouse;
+        return this;
+    }
+    public String getMemoria() {
+        return memoria;
+    }
+    public Computadora setMemoria(String memoria) {
+        this.memoria = memoria;
+        return this;
+    }
+    public String getProcesador() {
+        return procesador;
+    }
+    public Computadora setProcesador(String procesador) {
+        this.procesador = procesador;
+        return this;
+    }
+    public String getVideo() {
+        return video;
+    }
+    public Computadora setVideo(String video) {
+        this.video = video;
+        return this;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public Computadora setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public void showInfo(){
+        System.out.printl("Esta Usando lo siguiente")
+        System.out.println(nombre+"____________________________");
+        System.out.println("Monitor: "+monitor);
+        System.out.println("Teclado: "+teclado);
+        System.out.println("Mouse: "+mouse);
+        System.out.println("Memoria: "+memoria);
+        System.out.println("Procesador: "+procesador);
+        System.out.println("Video: "+video);
+    }
+    
+    
+}
