866bf63bc34e4352676d901b9521cf0586c660bc
AlexiaMarin <101999457+dis0400@users.noreply.github.com>
2023-02-15 10:09:40 -0800

    Create CompuI9_alto.java

---
+++ BuilderMethod/CompuI9_alto.java
@@ -0,0 +1,48 @@
+package BuilderMethod;
+
+public class CompuI9_alto extends BuilderCompuG {
+
+    
+
+    @Override
+    public void buildNombre() {
+        this.computadora.setNombre("I9-Alto");
+        
+    }
+
+    @Override
+    public void buildMemoria() {
+        this.computadora.setMemoria("32gb");
+    }
+
+    @Override
+    public void buildMonitor() {
+        this.computadora.setMonitor("50");
+        
+    }
+
+    @Override
+    public void buildMouse() {
+        this.computadora.setMouse("gamer");
+        
+    }
+
+    @Override
+    public void buildProcesador() {
+        this.computadora.setProcesador("i9");
+        
+    }
+
+    @Override
+    public void buildTeclado() {
+        this.computadora.setTeclado("wireless");
+        
+    }
+
+    @Override
+    public void buildVideo() {
+        this.computadora.setVideo("16");
+        
+    }
+    
+}
